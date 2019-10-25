1e701b531617554e55f3a3a3e06124a0d56a0bd4
Juha Wiberg <juha.wiberg@gmail.com>
2019-10-26 00:00:47 +0300

    16 ready.

--- src/koans/test.clj
+++ src/koans/test.clj
@@ -1,7 +1,13 @@
-(defn factorial [n]
-  (loop [n n acc n]
-    (if (= n 1)
-      acc
-      (recur (dec n) (* acc (dec n))))))
+(def test-address
+  {:street-address "123 Test Lane"
+   :city "Testerville"
+   :state "TX"})
 
-(factorial 10)
+((fn [[a b]] (str a " " b)) ["Test" "Testerson"]test-address)
+
+"Test Testerson, 123 Test Lane, Testerville, TX"
+ 
+(___ ["Test" "Testerson"] test-address))
+
+(let [{:keys [street-address :city :state]} test-address]
+       (str street-address ", " city ", " state)))
